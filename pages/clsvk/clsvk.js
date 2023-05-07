eeab6dc5c87c1580b572f51c24dae5562e35edf5
liaowentao <1421750225@qq.com>
2023-05-08 01:31:39 +0800

    尝试接three

--- pages/clsvk/clsvk.js
+++ pages/clsvk/clsvk.js
@@ -1,3 +1,7 @@
+import {createScopedThreejs} from 'threejs-miniprogram'
+import {registerGLTFLoader} from '../../loaders/gltf-loader'
+// import cloneGltf from '../../loaders/gltf-clone'
+
 const sysInfo = wx.getSystemInfoSync();
 
 Page({
@@ -8,7 +12,7 @@ Page({
     canvasT: 0,
     canvasL: 0,
     // 是否显示左上角的调试信息
-    showDebugPanel: false,
+    showDebugPanel: true,
 
     // // 新园区
     // clsConfig: {
@@ -27,9 +31,11 @@ Page({
 
     cameraWidth: `${sysInfo.windowWidth}px`,
     cameraHeight: `${sysInfo.windowHeight * 0.8}px`,
+    width: sysInfo.windowWidth,
+    height: sysInfo.windowHeight*0.98,
 
     running: true,
-    // minInterval: 1000, // 识别间隔
+    minInterval: 3000, // 识别间隔
     // motion: true,
     // showScan: false,
     load3d: false,
@@ -38,7 +44,7 @@ Page({
     sceneFileName: '1729120.json',
     // projectUrl: 'https://sightp-tour-cdn.sightp.com/wxapp/apps/Test/pdrjy_office_v1',
     // sceneFileName: '1434681.json',
-    // enhanceDevtools: sysInfo.platform === 'devtools',
+    enhanceDevtools: sysInfo.platform === 'devtools',
     // loadScripts,
     // loadNetworkScripts: {},
   },
@@ -47,6 +53,196 @@ Page({
       keepScreenOn: true,
     });
     this.setData({ load3d: true })
+  },
+  onUnload() {
+    if (this.renderer) {
+      this.renderer.dispose()
+      this.renderer = null
+    }
+    if (this.scene) {
+      this.scene.dispose()
+      this.scene = null
+    }
+    if (this.camera) this.camera = null
+    if (this.model) this.model = null
+    if (this._insertModels) this._insertModels = null
+    if (this.planeBox) this.planeBox = null
+    if (this.mixers) {
+      this.mixers.forEach(mixer => mixer.uncacheRoot(mixer.getRoot()))
+      this.mixers = null
+    }
+    if (this.clock) this.clock = null
+
+    if (this.THREE) this.THREE = null
+    if (this._program && this._program.gl) {
+      this._program.gl.deleteProgram(this._program)
+      this._program = null
+    }
+    if (this.canvas) this.canvas = null
+    if (this.session) this.session = null
+  },
+  onReady() {
+    wx.createSelectorQuery()
+      .select('#webgl')
+      .node()
+      .exec(res => {
+        this.canvas = res[0].node
+        this.initTHREE();
+        this.initGL();
+      })
+  },
+  initTHREE() {
+    const THREE = this.THREE = createScopedThreejs(this.canvas)
+    registerGLTFLoader(THREE)
+  
+    // 相机
+    this.camera = new THREE.Camera()
+  
+    // 场景
+    const scene = this.scene = new THREE.Scene()
+
+    // 光源
+    const light1 = new THREE.HemisphereLight(0xffffff, 0x444444) // 半球光
+    light1.position.set(0, 0.2, 0)
+    scene.add(light1)
+    const light2 = new THREE.DirectionalLight(0xffffff) // 平行光
+    light2.position.set(0, 0.2, 0.1)
+    scene.add(light2)
+
+    // 渲染层
+    const renderer = this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
+    renderer.gammaOutput = true
+    renderer.gammaFactor = 2.2
+  },
+  initGL() {
+    this.initShader()
+    this.initVAO()
+  },
+  initShader() {
+    const gl = this.renderer.getContext()
+    const currentProgram = gl.getParameter(gl.CURRENT_PROGRAM)
+    const vs = `
+      attribute vec2 a_position;
+      attribute vec2 a_texCoord;
+      uniform mat3 displayTransform;
+      varying vec2 v_texCoord;
+      void main() {
+        vec3 p = displayTransform * vec3(a_position, 0);
+        gl_Position = vec4(p, 1);
+        v_texCoord = a_texCoord;
+      }
+    `
+    const fs = `
+      precision highp float;
+
+      uniform sampler2D y_texture;
+      uniform sampler2D uv_texture;
+      varying vec2 v_texCoord;
+      void main() {
+        vec4 y_color = texture2D(y_texture, v_texCoord);
+        vec4 uv_color = texture2D(uv_texture, v_texCoord);
+
+        float Y, U, V;
+        float R ,G, B;
+        Y = y_color.r;
+        U = uv_color.r - 0.5;
+        V = uv_color.a - 0.5;
+        
+        R = Y + 1.402 * V;
+        G = Y - 0.344 * U - 0.714 * V;
+        B = Y + 1.772 * U;
+        
+        gl_FragColor = vec4(R, G, B, 1.0);
+      }
+    `
+    const vertShader = gl.createShader(gl.VERTEX_SHADER)
+    gl.shaderSource(vertShader, vs)
+    gl.compileShader(vertShader)
+
+    const fragShader = gl.createShader(gl.FRAGMENT_SHADER)
+    gl.shaderSource(fragShader, fs)
+    gl.compileShader(fragShader)
+
+    const program = this._program = gl.createProgram()
+    this._program.gl = gl
+    gl.attachShader(program, vertShader)
+    gl.attachShader(program, fragShader)
+    gl.deleteShader(vertShader)
+    gl.deleteShader(fragShader)
+    gl.linkProgram(program)
+    gl.useProgram(program)
+
+    const uniformYTexture = gl.getUniformLocation(program, 'y_texture')
+    gl.uniform1i(uniformYTexture, 5)
+    const uniformUVTexture = gl.getUniformLocation(program, 'uv_texture')
+    gl.uniform1i(uniformUVTexture, 6)
+
+    this._dt = gl.getUniformLocation(program, 'displayTransform')
+    gl.useProgram(currentProgram)
+  },
+  initVAO() {
+    const gl = this.renderer.getContext()
+    const ext = gl.getExtension('OES_vertex_array_object')
+    this._vao_ext = ext
+
+    const currentVAO = gl.getParameter(gl.VERTEX_ARRAY_BINDING)
+    const vao = ext.createVertexArrayOES()
+    
+    ext.bindVertexArrayOES(vao)
+
+    const posAttr = gl.getAttribLocation(this._program, 'a_position')
+    const pos = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, pos)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]), gl.STATIC_DRAW)
+    gl.vertexAttribPointer(posAttr, 2, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(posAttr)
+    vao.posBuffer = pos
+
+    const texcoordAttr = gl.getAttribLocation(this._program, 'a_texCoord')
+    const texcoord = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, texcoord)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([1, 1, 0, 1, 1, 0, 0, 0]), gl.STATIC_DRAW)
+    gl.vertexAttribPointer(texcoordAttr, 2, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(texcoordAttr)
+    vao.texcoordBuffer = texcoord
+
+    ext.bindVertexArrayOES(currentVAO)
+    this._vao = vao
+  },
+  renderGL(frame) {
+    const gl = this.renderer.getContext()
+    gl.disable(gl.DEPTH_TEST)
+    const {yTexture, uvTexture} = frame.getCameraTexture(gl, 'yuv')
+    const displayTransform = frame.getDisplayTransform()
+    if (yTexture && uvTexture) {
+      const currentProgram = gl.getParameter(gl.CURRENT_PROGRAM)
+      const currentActiveTexture = gl.getParameter(gl.ACTIVE_TEXTURE)
+      const currentVAO = gl.getParameter(gl.VERTEX_ARRAY_BINDING)
+
+      gl.useProgram(this._program)
+      this._vao_ext.bindVertexArrayOES(this._vao)
+
+      gl.uniformMatrix3fv(this._dt, false, displayTransform)
+      gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1)
+
+      gl.activeTexture(gl.TEXTURE0 + 5)
+      const bindingTexture5 = gl.getParameter(gl.TEXTURE_BINDING_2D)
+      gl.bindTexture(gl.TEXTURE_2D, yTexture)
+
+      gl.activeTexture(gl.TEXTURE0 + 6)
+      const bindingTexture6 = gl.getParameter(gl.TEXTURE_BINDING_2D)
+      gl.bindTexture(gl.TEXTURE_2D, uvTexture)
+
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+
+      gl.bindTexture(gl.TEXTURE_2D, bindingTexture6)
+      gl.activeTexture(gl.TEXTURE0 + 5)
+      gl.bindTexture(gl.TEXTURE_2D, bindingTexture5)
+
+      gl.useProgram(currentProgram)
+      gl.activeTexture(currentActiveTexture)
+      this._vao_ext.bindVertexArrayOES(currentVAO)
+    }
   },
   pause() {
     this.setData({ running: false });
@@ -69,7 +265,13 @@ Page({
         console.log('clsVK is not stable');
       }
     });
+    // this.threeScene = new threeScene(VKSessionContext.self.cameraCanvas);
   },
+  // onClsClientLoad(e) {
+  //   let { clsClientContext, clsCameraContext } = e.detail;
+  //   this.clsclient = e.detail.clsClientContext;
+  //   // console.log("onClsClientLoad", clsClientContext, clsCameraContext);
+  // },
   onResize(e) {
     this.setData(e.detail);
   },
@@ -94,18 +296,47 @@ Page({
    */
   onVKUpdate(e) {
     let data = e;//.detail;
-    if (this._app3d && this._camera3d && this._camera3d.camera) {
-      const projectionMatrix = data.vkCamera.getProjectionMatrix(this._camera3d.camera.nearClip, this._camera3d.camera.farClip);
-      let vp = new this._pc.Mat4().set(projectionMatrix);
-      this._camera3d.camera.projectionMatrix.set(vp.data);
+    // console.log('88888888888')
+    this.renderGL(data.frame);
+    // 开启下面的逻辑就会必然卡死 感觉是性能不够
+    // const camera = data.frame.camera
 
-      if (data.clsCameraPose) {
-        this._camera3d.worldTransform.set(data.clsCameraPose);
-      } else {
-        this._camera3d.worldTransform.set(data.vkCameraPose);
-      }
-    }
+    // // 相机
+    // if (camera) {
+    //   this.camera.matrixAutoUpdate = false
+    //   this.camera.matrixWorldInverse.fromArray(camera.viewMatrix)
+    //   this.camera.matrixWorld.getInverse(this.camera.matrixWorldInverse)
+
+    //   const projectionMatrix = camera.getProjectionMatrix(NEAR, FAR)
+    //   this.camera.projectionMatrix.fromArray(projectionMatrix)
+    //   this.camera.projectionMatrixInverse.getInverse(this.camera.projectionMatrix)
+    // }
+
+    // this.renderer.autoClearColor = false
+    // this.renderer.render(this.scene, this.camera)
+    // this.renderer.state.setCullFace(this.THREE.CullFaceNone)
+
+
+    // if (this._app3d && this._camera3d && this._camera3d.camera) {
+    //   // console.log('7777777777777')
+    //   const projectionMatrix = data.vkCamera.getProjectionMatrix(this._camera3d.camera.nearClip, this._camera3d.camera.farClip);
+    //   let vp = new this._pc.Mat4().set(projectionMatrix);
+    //   this._camera3d.camera.projectionMatrix.set(vp.data);
+
+    //   if (data.clsCameraPose) {
+    //     this._camera3d.worldTransform.set(data.clsCameraPose);
+    //   } else {
+    //     this._camera3d.worldTransform.set(data.vkCameraPose);
+    //   }
+    // }
   },
+  // onClsClientResult(e) {
+  //   // console.log("onClsClientResult", e.detail);
+  //   this.setData({
+  //     result: e.detail.msg,
+  //     count: this.data.count + 1
+  //   });
+  // },
   onClsClientResult(e) {
     if (e.detail.statusCode == 0) {
       // console.log("onClsClientResult", e.detail);
