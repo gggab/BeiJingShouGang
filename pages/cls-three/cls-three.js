ef9150d469a650ac2bbe0dead21b4e0ff1e63a9d
钱梦玉 <qmy@qianmengyudeMacBook-Pro.local>
2023-05-13 12:56:06 +0800

    之后一页添加下载视频按钮,下载视频到本地

--- pages/cls-three/cls-three.js
+++ pages/cls-three/cls-three.js
@@ -427,6 +427,67 @@ Page({
   onShareAppMessage() {
 
   },
+  /**
+   * 下载视频并保存到相册
+   */
+  downloadVideoHandler() {
+    let link = "https://ball.forgenius.cn/video.mp4";//e.currentTarget.dataset.link;
+    let fileName = new Date().valueOf();
+    wx.downloadFile({
+      url: link,
+      filePath: wx.env.USER_DATA_PATH + '/' + fileName + '.mp4',
+      success: res => {
+        console.log(res);
+        let filePath = res.filePath;
+        wx.saveVideoToPhotosAlbum({
+          filePath,
+          success: file => {
+            wx.showToast({
+              title: '下载成功',
+              icon: 'success',
+              duration: 2000
+            })
+            let fileMgr = wx.getFileSystemManager();
+            fileMgr.unlink({
+              filePath: wx.env.USER_DATA_PATH + '/' + fileName + '.mp4',
+              success: function (r) {
+
+              },
+            })
+          },
+          fail: err => {
+            console.log(err)
+            if (err.errMsg === 'saveVideoToPhotosAlbum:fail auth deny') {
+              wx.showModal({
+                title: '提示',
+                content: '需要您授权保存相册',
+                showCancel: false,
+                success: data => {
+                  wx.openSetting({
+                    success(settingdata) {
+                      if (settingdata.authSetting['scope.writePhotosAlbum']) {
+                        wx.showModal({
+                          title: '提示',
+                          content: '获取权限成功,再次点击下载即可保存',
+                          showCancel: false,
+                        })
+                      } else {
+                        wx.showModal({
+                          title: '提示',
+                          content: '获取权限失败，将无法保存到相册哦~',
+                          showCancel: false,
+                        })
+                      }
+                    },
+                  })
+                }
+              })
+            }
+          }
+        })
+      }
+    })
+  },
   analysis() {
     if (!this._lastTs) this._lastTs = Date.now()
     const ts = Date.now()
