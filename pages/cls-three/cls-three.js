68ed5701180b3bbb612a48a7b5628957a099545c
liaowentao <1421750225@qq.com>
2023-05-13 11:39:03 +0800

    ios下静音也播放音频

--- pages/cls-three/cls-three.js
+++ pages/cls-three/cls-three.js
@@ -239,7 +239,10 @@ Page({
   playAudio() {
     if (this.isAudioPlaying) return;
     this.isAudioPlaying = true;
-
+    // （仅在 iOS 生效）是否遵循静音开关
+    wx.setInnerAudioOption({
+      obeyMuteSwitch: false
+    });
     const innerAudioContext = wx.createInnerAudioContext({
       useWebAudioImplement: false // 是否使用 WebAudio 作为底层音频驱动，默认关闭。对于短音频、播放频繁的音频建议开启此选项，开启后将获得更优的性能表现。由于开启此选项后也会带来一定的内存增长，因此对于长音频建议关闭此选项
     })
